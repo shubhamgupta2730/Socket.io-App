9c18aa67ff0b95cac405d91804e2e769c2d925b5
Shubham Gupta <shubhamgupta2730@gmail.com>
2024-06-26 07:52:25 +0530

    client-server-communication

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+app.get('/', (req, res)=>{
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket)=>{
+  console.log('A user connected');
+
+  //sending event to client: 
+  //socket.on Listens to an event 'chat message' sent by client: 
+  //io.emit sends message to all connected clients
+  socket.on('chat message', (msg)=>{
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', ()=>{
+    console.log('User disconnected');
+  })
+})
+
+
 const port= 3000;
-
-
-
 server.listen(port, ()=>{
-  console.log(`${port}`);
+  console.log(`Listening on port: ${port}`);
 });
